cb838732c8133879fecf032316b32ba8c03a9cc5
Grind <bellenlowell@gmail.com>
2023-04-28 04:34:45 +0800

    Commit files

---
+++ index.js
@@ -0,0 +1,30 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let firstPassword = document.getElementById("first-password")
+let secondPassword = document.getElementById("second-password")
+console.log(firstPassword.textContent)
+
+function generatePassword(){
+firstPassword.textContent = ""
+secondPassword.textContent = ""
+getRandomNum()
+getRandomNum2()
+}
+
+function getRandomNum(){
+    for(let i = 0; i < 15; i++){
+        let randomNum = (Math.floor(Math.random() * characters.length))
+        firstPassword.textContent += characters[randomNum]
+        }
+}
+
+function getRandomNum2() {
+    for(let i = 0; i < 15; i++){
+        let randomNum = (Math.floor(Math.random() * characters.length))
+        secondPassword.textContent += characters[randomNum]
+        }
+}
+
+
+
